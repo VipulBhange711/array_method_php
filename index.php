356d56c24165067a7e25ba4bfa704f9a00c7da0b
Vipul Bhange <vipulbhange4164@gmail.com>
2024-09-05 22:32:07 +0530

    all done

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 $array = ['Salman','Garvik','Mariya','Janvi','Sunaina'];
-$string = "Salman Garvik Mariya  Janvi  Sunaina";
+$string = "Salman Garvik Mariya Janvi Sunaina";
 
 echo "<h2>This is array method with Question</h2>" ;
 
@@ -13,6 +13,76 @@ echo "<h2>This is array method with Question</h2>" ;
 //Q2 :=  How we can remove any value from an array in any prosition ?
 // unset($array[2]);
 // print_r($array);
+
+//Q3 :=  How we add any value at the end of an array ;
+// array_push($array,"Kajal");
+// print_r($array);
+
+//Q4 :=  How we remove value at the end of an array ;
+// array_pop($array);
+// print_r($array);
+
+//Q5 :=  How we see only key of an array  ;
+// $arr =  array_keys($array);
+// print_r($arr);
+
+//Q6 :=  How we Convert any array to string ;
+// print_r($array);
+//   $arr =   implode(" ",$array);
+// print_r($arr);
+
+//Q7 := How we Convert any string to array ;
+// $arr = explode(" ", $string);
+// print_r($arr);
+
+//Q8 := How we merge two or more array;
+// $array2 = [1,2,3,4,5,6];
+// $arr = array_merge($array,$array2);
+// print_r($arr);
+
+//Q9 := How we remove duplicate element or values in an array ;
+// $arr = [1,2,3,3,4,5,6,1,2];
+// $arr1 = array_unique($arr);
+// print_r($arr1);
+
+//Q10 := How we count element in an array 
+// $arr = count($array);
+// print_r($arr);
+
+//Q11 := How we sort element in an array by assending order ?
+// $arr = [1,2,3,3,4,5,6,1,2,4,5,9,0];
+//  sort($arr); // by sort function
+// print_r($arr);
+
+
+//Q12 := How we sort element in an array by desending order ?
+// $arr = [1,2,3,3,4,5,6,1,2,4,5,9,0];
+//  rsort($arr);
+// print_r($arr);
+
+//Q13 := How we check if the specified key are exists in the array or not  ?
+// $arr =  array_key_exists('4',$array);
+// print_r($arr);
+
+//Q14 := How we can convert  array in reverse order ?
+// $arr = array_reverse($array);
+// print_r($arr);
+
+//Q15 := How we can  search an array for a given value and return the key
+// $arr = array_search("Salman",$array);
+// print_r($arr);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
