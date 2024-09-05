7e7523a3093102f96bcebecc7395c6fb9ef03791
Vipul Bhange <vipulbhange4164@gmail.com>
2024-09-05 21:58:22 +0530

    First Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+$array = ['Salman','Garvik','Mariya','Janvi','Sunaina'];
+$string = "Salman Garvik Mariya  Janvi  Sunaina";
+
+echo "<h2>This is array method with Question</h2>" ;
+
+//Q1 :=  How we find a perticular variable are array or not ?
+// print_r($array);
+// print_r(is_array($array));
+
+//Q2 :=  How we can remove any value from an array in any prosition ?
+// unset($array[2]);
+// print_r($array);
+
+
+
+?>
